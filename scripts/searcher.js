705a94e316f6f89fdc06ea7c92d1193f4e004668
Gleb Karzhavin <karzhav1n@yandex.ru>
2023-01-03 14:56:30 +0500

    Add styles for input line and result list, change listItem.addEventListener

--- scripts/searcher.js
+++ scripts/searcher.js
@@ -4,7 +4,7 @@
 
 async function getBooksData(title) {
     try {
-        const response = await fetch(`https://openlibrary.org/search.json?q=${title}&fields=key,title&limit=10`, {
+        const response = await fetch(`https://openlibrary.org/search.json?q=${title}&fields=*&limit=10`, {
             method: 'GET',
         });
         if (!response.ok) {
@@ -17,7 +17,7 @@ async function getBooksData(title) {
     }
 }
 
-async function getBook(key) {
+async function getBookInfo(key) {
     try {
         const response = await fetch(`https://openlibrary.org${key}.json`, {
             method: 'GET',
@@ -37,16 +37,19 @@ async function getBook(key) {
  */ 
 
 const searchLine = document.querySelector('.search-view__line');
+searchLine.focus();
 
 searchLine.addEventListener('input', (event) => {
     (async function() {
         const input = searchLine.value.replace(/\s+/g, '+');
         const result = await getBooksData(input);
+        console.log(result);
         const books = [];
 
         result.docs.forEach((book) => {
             books.push([book.key, book.title]);
         });
+
         createListOfBooks(books);
     })();
 });
@@ -61,14 +64,27 @@ function createListOfBooks(books) {
 
   for (const book of newList) {
     const listItem = document.createElement('li');
+    listItem.classList.add('list__item');
     listItem.classList.add('search-view__result-item');
     listItem.textContent = book[1];
     listItem.addEventListener('mouseover', () => {
-        const descriptionView = document.querySelector('.search-view__description-of-search-pick');
         (async function() {
-            const bookDescription = await getBook(book[0]);
-            // Инфа о книге уже извлечена, нужно допилить отображение и добавить каринку!!!
-            console.log(bookDescription.description);
+            const image = document.querySelector('.search-view__item-image');
+            const title = document.querySelector('.search-view__item-title');
+            const author = document.querySelector('.search-view__item-author');
+            const year = document.querySelector('.search-view__item-year');
+            const description = document.querySelector('.search-view__item-description');
+
+            const info = await getBookInfo(book[0]);
+
+            console.log(info);
+
+            if (info.covers) {
+                image.src = `https://covers.openlibrary.org/b/id/${info.covers[0]}-M.jpg`;
+            }
+            
+            title.textContent = info.title;
+            description.textContent = info.description;
         })();
     });
     resultList.appendChild(listItem);
