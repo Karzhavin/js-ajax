4456ac1a651035c5c1f51a87d9361bbead27dd25
Gleb Karzhavin <karzhav1n@yandex.ru>
2023-01-09 16:03:39 +0500

    Beta version of book searcher

--- scripts/searcher.js
+++ scripts/searcher.js
@@ -4,7 +4,7 @@
 
 async function getBooksData(title) {
     try {
-        const response = await fetch(`https://openlibrary.org/search.json?q=${title}&fields=*&limit=10`, {
+        const response = await fetch(`https://openlibrary.org/search.json?q=${title}&fields=key,title&limit=10`, {
             method: 'GET',
         });
         if (!response.ok) {
@@ -22,6 +22,7 @@ async function getBookInfo(key) {
         const response = await fetch(`https://openlibrary.org${key}.json`, {
             method: 'GET',
         });
+        console.log(response);
         if (!response.ok) {
             throw new Error(`HTTP error: ${response.status}`);
         }
@@ -37,25 +38,40 @@ async function getBookInfo(key) {
  */ 
 
 const searchLine = document.querySelector('.search-view__line');
+const separator = document.querySelector('.search-view__separator');
+const popUp = document.querySelector('.search-view__pop-up');
 searchLine.focus();
 
-searchLine.addEventListener('input', (event) => {
+searchLine.addEventListener('input', () => {
+    if (searchLine.value) {
+        separator.classList.add('search-view__separator_visible');
+        popUp.classList.add('search-view__pop-up_visible');
+    } else {
+        separator.classList.remove('search-view__separator_visible');
+        popUp.classList.remove('search-view__pop-up_visible');
+    }
     (async function() {
         const input = searchLine.value.replace(/\s+/g, '+');
         const result = await getBooksData(input);
-        console.log(result);
         const books = [];
 
         result.docs.forEach((book) => {
             books.push([book.key, book.title]);
         });
-
         createListOfBooks(books);
     })();
 });
 
+const resultList = document.querySelector('.search-view__result-list');
+const detailedInformation = document.querySelector('.search-view__detailed-information');
+
+const bookImage = document.querySelector('.search-view__item-image');
+const bookTitle = document.querySelector('.search-view__item-title');
+const bookAuthor = document.querySelector('.search-view__item-author');
+const bookYear = document.querySelector('.search-view__item-year');
+const bookDescription = document.querySelector('.search-view__item-description');
+
 function createListOfBooks(books) {
-  const resultList = document.querySelector('.search-view__result-list');
   const newList = books.slice();
 
   while (resultList.firstChild) {
@@ -68,24 +84,33 @@ function createListOfBooks(books) {
     listItem.classList.add('search-view__result-item');
     listItem.textContent = book[1];
     listItem.addEventListener('mouseover', () => {
+        detailedInformation.classList.add('search-view__detailed-information_visible');
         (async function() {
-            const image = document.querySelector('.search-view__item-image');
-            const title = document.querySelector('.search-view__item-title');
-            const author = document.querySelector('.search-view__item-author');
-            const year = document.querySelector('.search-view__item-year');
-            const description = document.querySelector('.search-view__item-description');
-
             const info = await getBookInfo(book[0]);
-
-            console.log(info);
-
             if (info.covers) {
-                image.src = `https://covers.openlibrary.org/b/id/${info.covers[0]}-M.jpg`;
+                bookImage.src = `https://covers.openlibrary.org/b/id/${info.covers[0]}-M.jpg`;
+            } else {
+                bookImage.src = 'styles/blocks/search-view/none_image.png';
             }
-            
-            title.textContent = info.title;
-            description.textContent = info.description;
+            console.log(info);
+            if (info.title) {
+                bookTitle.textContent = info.title;
+            } else {
+                bookTitle.textContent = 'none title';
+            }
+            if (info.description) {
+                if (typeof info.description === 'object') {
+                    bookDescription.textContent = info.description.value;
+                } else {
+                    bookDescription.textContent = info.description;
+                }
+            } else {
+                bookDescription.textContent = 'none description';
+            }
         })();
+    });
+    listItem.addEventListener('mouseout', () => {
+        detailedInformation.classList.remove('search-view__detailed-information_visible');
     });
     resultList.appendChild(listItem);
   }
